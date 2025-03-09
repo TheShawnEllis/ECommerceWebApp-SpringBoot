ac3c0344afd84475973f18f747d5379042db60e7
DeShawn E. <65631445+TheShawnEllis@users.noreply.github.com>
2025-03-09 10:32:27 -0700

    Initial project setup commit.

---
+++ src/main/java/learning/springframwork/ecommercewebapp/EcommercewebappApplication.java
@@ -0,0 +1,13 @@
+package learning.springframwork.ecommercewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercewebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercewebappApplication.class, args);
+	}
+
+}
